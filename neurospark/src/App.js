b12e5af3bff28891549438093b95859cbfd9fd5a
benyedder38 <benyad23q@gmail.com>
2024-10-22 23:17:33 +1100

    navbar functionality implemented

---
+++ neurospark/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { 
+  BrowserRouter as Router, 
+  Routes, 
+  Route
+} from 'react-router-dom';
+import Navbar from './components/Navbar';
+import './App.css';
+
+function App() {
+  return (
+    <>
+     <Router>
+      <Navbar />
+      <Routes>
+        <Route path='/' exacts/>
+      </Routes>
+     </Router>
+    </>
+  );
+}
+
+export default App;
---
+++ neurospark/src/components/Navbar.jsx
@@ -0,0 +1,70 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Button } from './Button';
+import './Navbar.css'
+
+function NavBar() {
+  const [click, setClick] = React.useState(false);
+  const [button, setButton] = React.useState(true);
+
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+  const showButton = () => {
+    if (window.innerWidth <= 960) {
+      setButton(false)
+    } else {
+      setButton(true)
+    }
+  };
+
+  React.useEffect(() => {
+    showButton()
+  }, []);
+
+  window.addEventListener('resize', showButton);
+
+  return (
+    <>
+      <nav className="navbar">
+        <div className="navbar-container">
+          <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
+            NEUROSPARK
+          </Link>
+          <div className="menu-icon" onClick={handleClick}>
+            <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+          </div>
+          <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+            <li className='nav-item'>
+              <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+                Home
+              </Link>
+            </li>
+            <li className='nav-item'>
+              <Link to='/services' className='nav-links' onClick={closeMobileMenu}>
+                Services
+              </Link>
+            </li>
+            <li className='nav-item'>
+              <Link to='/projects' className='nav-links' onClick={closeMobileMenu}>
+                Projects
+              </Link>
+            </li>
+            <li className='nav-item'>
+              <Link to='/contact' className='nav-links' onClick={closeMobileMenu}>
+                Contact
+              </Link>
+            </li>
+            <li className='nav-item'>
+              <Link to='/sign-up' className='nav-links-mobile' onClick={closeMobileMenu}>
+                Sign Up
+              </Link>
+            </li>
+          </ul>
+          {button && <Button buttonStyle='btn--outline'>SIGN UP</Button>}
+        </div>
+      </nav>
+    </>
+  )
+}
+
+export default NavBar
