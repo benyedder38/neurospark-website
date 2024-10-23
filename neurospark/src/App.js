0f6bdeeea510c697c10ac1bf6ff9ec7937eb3400
benyedder38 <benyad23q@gmail.com>
2024-10-23 23:06:21 +1100

    basic template of site complete

--- neurospark/src/App.js
+++ neurospark/src/App.js
@@ -6,6 +6,11 @@ import {
 } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import './App.css';
+import Home from './pages/Home';
+import Services from './pages/Services'
+import Projects from './pages/Projects'
+import Contact from './pages/Contact'
+import Signup from './pages/Signup'
 
 function App() {
   return (
@@ -13,7 +18,11 @@ function App() {
      <Router>
       <Navbar />
       <Routes>
-        <Route path='/' exacts/>
+        <Route path='/' element={<Home />} />
+        <Route path='/services' element={<Services />} />
+        <Route path='/projects' element={<Projects />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/sign-up' element={<Signup />} />
       </Routes>
      </Router>
     </>
---
+++ neurospark/src/components/Cards.jsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import CardItem from './CardItem'
+import './Cards.css'
+
+function Cards() {
+  return (
+    <>
+      <div className="cards">
+        <h1>Check out our projects</h1>
+        <div className="cards__container">
+          <div className="cards__wrapper">
+            <ul className="cards__items">
+              <CardItem 
+                src='images/img-1.jpg'
+                text='Check out our project at obeid house'
+                label='Projects'
+                path='/projects'
+              />
+              <CardItem 
+                src='images/img-2.jpg'
+                text='Check out our project at walid house'
+                label='Projects'
+                path='/projects'
+              />
+            </ul>
+            <ul className="cards__items">
+              <CardItem 
+                src='images/img-9.jpg'
+                text='Check out our project at firdaus house'
+                label='Projects'
+                path='/projects'
+              />
+              <CardItem 
+                src='images/img-1.jpg'
+                text='Check out our project at obeid house'
+                label='Projects'
+                path='/projects'
+              />
+              <CardItem 
+                src='images/img-2.jpg'
+                text='Check out our project at walid house'
+                label='Projects'
+                path='/projects'
+              />
+            </ul>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Cards
